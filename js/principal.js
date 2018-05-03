4da3c2beb185ad1632b84776dc94b1d13ed1b34e
Santiago Llull Sesma <sls1@abg.corp>
2018-05-03 11:52:06 +0200

    cargar login sin estilos

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,18 @@
-var bCargarLogin= false;
-$("#login")
+$("#btnLogin").click(mostrarLogin);
+
+function mostrarFormularios(sForm){
+    $("#formLogin form:not('#"+sForm+"')").hide("normal");
+    $("#formLogin").show("normal");
+}
+
+function mostrarLogin(){
+    mostrarFormularios("frmLogin");
+
+    if ($('#frmLogin').length == 0) {
+        $("<div>").appendTo('#formLogin').load("paginas/login.html", function(){
+            alert("HOLA");
+        });
+    } else{
+        $('#frmLogin').show("normal");
+    }
+}
