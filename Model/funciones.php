436f0d15918b030793dbe65cb614d887c3581460
alenunez1208 <alenunezcdm@gmail.com>
2018-05-14 14:36:42 +0200

    panelAdmin

--- Model/funciones.php
+++ Model/funciones.php
@@ -7,7 +7,7 @@
                 $opc=array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
                 $dsn="mysql:host=localhost;dbname=carquinto";
                 $usuario="root";
-                $contrasena="";
+                $contrasena="root";
                 $base=new PDO($dsn,$usuario,$contrasena,$opc);
             } catch (PDOException $e) {
                 die ("Error".$e->getMessage());
@@ -24,7 +24,7 @@
         }
 
         public static function obtieneUsuario($usuario,$password){
-            $sql= "SELECT nombre,apellidos,email,password,telefono,tipo FROM usuarios WHERE email='".$usuario."' AND password='".$password."'";
+            $sql= "SELECT nombre,apellidos,email,password,telefono,tipo FROM usuarios WHERE email='".$usuario."' AND password='".$password."' AND activo=true";
             $resultset= self::ejecutaConsulta($sql);
             $row= $resultset->fetch(PDO::FETCH_ASSOC);
             $usuario= new Usuario($row);
