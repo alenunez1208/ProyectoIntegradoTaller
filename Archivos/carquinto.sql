08188a9323d8fc8a0c822b56b7d74178a44d3357
alenunez1208 <alenunezcam@gmail.com>
2018-05-22 22:43:44 +0200

    bbdd

--- Archivos/carquinto.sql
+++ Archivos/carquinto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-05-2018 a las 18:08:24
+-- Tiempo de generación: 22-05-2018 a las 22:37:50
 -- Versión del servidor: 10.1.28-MariaDB
 -- Versión de PHP: 7.1.11
 
@@ -21,6 +21,48 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `carquinto`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `id` int(3) NOT NULL,
+  `asunto` varchar(50) NOT NULL,
+  `fecha` date NOT NULL,
+  `fecha_esp` date NOT NULL,
+  `hora` varchar(5) NOT NULL,
+  `motivo` varchar(500) NOT NULL,
+  `estado` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horas`
+--
+
+CREATE TABLE `horas` (
+  `hora` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `horas`
+--
+
+INSERT INTO `horas` (`hora`) VALUES
+('07:00'),
+('08:00'),
+('09:00'),
+('10:00'),
+('11:00'),
+('12:00'),
+('13:00'),
+('14:00'),
+('15:00'),
+('16:00');
 
 -- --------------------------------------------------------
 
@@ -47,15 +89,39 @@ INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `email`, `password`, `telef
 (1, 'admin', 'admin', 'admin', 'admin', 666666666, 'admin', 1),
 (2, 'Alejandro', 'Nunez', 'ale@gmail.com', 'root', 655388778, 'usuario', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_citas`
+--
+
+CREATE TABLE `usuarios_citas` (
+  `id_usuario` int(3) NOT NULL,
+  `id_cita` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios_citas`
+--
+ALTER TABLE `usuarios_citas`
+  ADD KEY `FK_UsuarioId` (`id_usuario`),
+  ADD KEY `FK_CitaId` (`id_cita`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -66,6 +132,17 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `usuarios_citas`
+--
+ALTER TABLE `usuarios_citas`
+  ADD CONSTRAINT `FK_CitaId` FOREIGN KEY (`id_cita`) REFERENCES `citas` (`id`),
+  ADD CONSTRAINT `FK_UsuarioId` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Model/funciones.php
+++ Model/funciones.php
@@ -7,7 +7,7 @@
                 $opc=array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
                 $dsn="mysql:host=localhost;dbname=carquinto";
                 $usuario="root";
-                $contrasena="root";
+                $contrasena="";
                 $base=new PDO($dsn,$usuario,$contrasena,$opc);
             } catch (PDOException $e) {
                 die ("Error".$e->getMessage());
